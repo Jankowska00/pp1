776e2705785eeff174ffc19d56bd29f0a6049249
s-115-27 <s-115-27@gumiforest.ae.krakow.pl>
2019-12-09 11:52:54 +0100

    zadania

---
+++ 07-ObjectOrientedProgramming/zadanie 5.py
@@ -0,0 +1,12 @@
+class utwór():
+    
+    def __init__(self, wykonawca, tytuł, album, rok):
+        self.wykonawca = wykonawca
+        self.tytuł = tytuł
+        self.album = album
+        self.rok = rok
+        
+    def __str__(self):
+        return 'Wykonawca: ',{}.format(self.wykonawca)/n
+
+        
