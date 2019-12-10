c03aafa7ec4badcab9495f499ecfd1218a11e6a3
s-301-12 <s-301-12@gumiforest.uek.krakow.pl>
2019-12-10 15:18:25 +0100

    poprawka 35

--- 02-ControlStructures/zadanie 35.py
+++ 02-ControlStructures/zadanie 35.py
@@ -2,17 +2,16 @@ a = int(input('Podaj a: '))
 b = int(input('Podaj b: '))
 c = int(input('Podaj c: '))
 
-
-import cmath
+import math
 
 delta = b**2-(4*a*c)
 if delta > 0:
-    p_z_delty = cmath.sqrt(delta)
-    pierwiastek1 = ((-b)+(p_z_delty))/2*a
-    pierwiastek2 = ((-b)-(p_z_delty))/2*a
+    p_z_delty = math.sqrt(delta)
+    pierwiastek1 = ((-b)+(p_z_delty))/(2*a)
+    pierwiastek2 = ((-b)-(p_z_delty))/(2*a)
     print(f'Pierwiastek = {pierwiastek1} v {pierwiastek2}')
 elif delta ==0:
-    pierwiastek1 = (-b)/2*a
+    pierwiastek1 = (-b)/(2*a)
     print(f'Pierwiastek = {pierwiastek1}')
 if delta < 0:
      print(f'Funkcja nie ma pierwiastków.')
